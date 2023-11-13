ca13194eb51bee96f8cc49ad6d67a36f8323e394
Cédric <cedric.wyrsch@bluewin.ch>
2023-11-13 20:19:52 +0100

    commit test

---
+++ test_cloundinit_spring/src/main/java/com/example/test_cloundinit_spring/TestCloundinitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_cloundinit_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCloundinitSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCloundinitSpringApplication.class, args);
+    }
+
+}
---
+++ test_cloundinit_spring/src/test/java/com/example/test_cloundinit_spring/TestCloundinitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_cloundinit_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCloundinitSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
